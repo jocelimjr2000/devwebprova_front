7a6a5c495144c2b14b71d77f9689882c70d1ac6c
Jocelim <jocelimjr2000@hotmail.com>
2020-11-26 21:40:12 -0300

    formulario

--- src/app/view/vehicle/create/create.component.ts
+++ src/app/view/vehicle/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { VehicleService } from "src/app/services/vehicle.service";
+import { Vehicle } from "src/app/models/vehicle";
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  vehicle: Vehicle = new Vehicle();
+
+  modelo: string;
+  placa: string;
+  ano: number;
+
+  constructor(
+    private service: VehicleService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  create(): void {
+    this.vehicle.modelo = this.modelo;
+    this.vehicle.placa = this.placa;
+    this.vehicle.ano = this.ano;
+
+    this.service.create(this.vehicle).subscribe(
+      (result) => {
+        this.router.navigate([""]);
+      },
+      (error) => {
+        if (error.status === 403) {
+          this.service.showMessage("Placa já cadastrada");
+        } else {
+          this.service.showMessage("Verifique os dados preenchidos");
+        }
+      }
+    );
+  }
 }
